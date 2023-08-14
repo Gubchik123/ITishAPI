6349790453154dd2bb02ab61c7deea7d79f8a7a5
Nikita <nikita.gubarev31@gmail.com>
2023-08-14 11:35:46 +0300

    Add CORS middleware

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from db import Base, engine
 from blog.router import blog_router
@@ -13,7 +14,13 @@ app = FastAPI(
     description="API for ITish blog site",
     version="1.0.0",
 )
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(blog_router, prefix="/api/blog", tags=["blog"])
 app.include_router(user_router, prefix="/api/user", tags=["user"])
 app.include_router(auth_router, prefix="/api/auth", tags=["auth"])
